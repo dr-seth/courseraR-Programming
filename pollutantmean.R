2bd8c01fb7e6a09d36896acee115cac6759eca0e
Seth Dobrin <sedobrin@gmail.com>
2015-12-12 19:20:36 -0600

    Added comments

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,13 +1,12 @@
 pollutantmean<-function(directory,pollutant,id=1:332){
-  directory<-"~/R-wd/specdata"
-  setwd(directory)   
-  files<-dir()
-  data<-data.frame()
-  for(i in id)
+  directory<-"~/R-wd/specdata"                              #specify working directory
+  setwd(directory)                                          #set working directory
+  fl<-dir()                                                 #specify files
+  da<-data.frame()                                          #specify data as dataframe
+  for(i in id)                                              #read thru all data files
   {
-    data<-rbind(data,read.csv(files[i]))
+    da<-rbind(da,read.csv(fl[i]))                           #read specified files
   }
-  mean<-round(mean(data[,pollutant],na.rm=TRUE), digits = 3)
-  setwd("~/R-wd/specdata")
-  mean
+  mean<-round(mean(da[,pollutant],na.rm=TRUE), digits = 3)  #calculate mean and round to 3 decimals
+  mean                                                      #print the mean
 }
